3b7087f84287f6a07dfb24dc34ff0945eee1c7dd
Yaroslav Romachenko <yaroslav.r1996@gmail.com>
2025-12-13 15:49:28 +0300

    Final version

--- src/Controller/PasswordController.php
+++ src/Controller/PasswordController.php
@@ -7,14 +7,20 @@ use App\Form\GeneratePasswordType;
 use App\Library\FormCreator;
 use App\Service\PasswordGenerateService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/password')]
 final class PasswordController extends AbstractController
 {
-    #[Route('/generate', name: 'generate_password', methods: ['GET','POST'])]
+    #[Route('/', name: 'homepage', methods: ['GET'])]
+    public function redirectToPassword(): RedirectResponse
+    {
+        return $this->redirectToRoute('generate_password');
+    }
+
+    #[Route('/generate-password', name: 'generate_password', methods: ['GET','POST'])]
     public function generate(
         Request $request,
         FormCreator $formCreator,
